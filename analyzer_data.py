e2b1397b3251051e275d89232bd81f90ed760354
Allison-LiBear <allisoncastellanos8@gmail.com>
2023-05-28 20:55:08 +0200

    functions to return graphs implemented

--- analyzer_data.py
+++ analyzer_data.py
@@ -6,6 +6,7 @@ import numpy as np
 import requests
 import all_scrapper
 
+
 def get_type_word(word_to_search):
     r = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word_to_search}')
     if r.status_code == 200:
@@ -13,6 +14,7 @@ def get_type_word(word_to_search):
         for info in info_api_dict[0]['meanings']:
             # dict_keys(['partOfSpeech', 'definitions', 'synonyms', 'antonyms'])
             return info['partOfSpeech']
+
 
 def get_n_top_words(filename, n_words, keyword):
     df = pd.DataFrame()
@@ -37,8 +39,8 @@ def get_n_top_words(filename, n_words, keyword):
     df = df[~df['words'].isin(common_words)]
 
     # clean keyword topic
-    #index_keyword = df.index[df['words'] == keyword].tolist()[0]
-    #df.drop(index_keyword, inplace=True)
+    # index_keyword = df.index[df['words'] == keyword].tolist()[0]
+    # df.drop(index_keyword, inplace=True)
 
     # sort by importance
     df = df.sort_values(['TF'])
@@ -49,15 +51,48 @@ def get_n_top_words(filename, n_words, keyword):
         result_df.loc[result_df['words'] == word, 'type'] = get_type_word(word)
     return result_df
 
+
 def top_words_gui_getter(topic: str, n: int, source: int):
     if source == 0:
         all_scrapper.wikipedia_parser(topic)
-        return get_n_top_words(f"wikipedia_{topic}.txt",n,"")
+        return get_n_top_words(f"wikipedia_{topic}.txt", n, "")
     elif source == 1:
         all_scrapper.search_all_subreddit(topic)
-        return get_n_top_words(f"reddit_{topic}.txt",n,"")
+        return get_n_top_words(f"reddit_{topic}.txt", n, "")
     elif source == 2:
         all_scrapper.youtube_parser(topic)
-        return get_n_top_words(f"youtube_{topic}.txt",n,"")
-    
-print(top_words_gui_getter("Bee",5,0)['words'].values.tolist())
+        return get_n_top_words(f"youtube_{topic}.txt", n, "")
+
+
+def pie_chart_individual_source(source_df: pd.DataFrame, source_from: str) -> plt.figure:
+    """
+    :param source_df: dataframe containing the most n popular words from a specific source
+    :param source_from: string used for title indicating where the data is parsed
+    :return: figure to use in gui.py
+    """
+    labels = source_df['words']
+    TF_value = source_df['TF']
+
+    fig, ax = plt.subplots()
+    plt.title(f"TF-IDF from {source_from}")
+    return ax.pie(TF_value, labels=labels, autopct='%1.1f%%')
+
+
+def bar_chart_all_sources(all_df: pd.DataFrame) -> plt.figure:
+    """
+    :param all_df: concatenate datafrmaes from all sources chosen
+    :return: bar char with type of word frequency
+    """
+    series_words = all_df['type'].value_counts()
+
+    # creating the dataset
+    fig = plt.figure(figsize=(5, 5))
+
+    # creating the bar plot
+    plt.bar(series_words.index, series_words.values, color='green',
+            width=0.4)
+
+    plt.xlabel("Word type")
+    plt.ylabel("Frequency")
+    plt.title("Frequency of word types")
+    return fig
